c10c399bb93c3ee6f12fe2ac27647059fddf808e
Mansolo-21 <solomonjohari@gmail.com>
2026-05-30 10:18:20 +0300

    more

--- static/js/main.js
+++ static/js/main.js
@@ -14,3 +14,51 @@ window.addEventListener("load", () => {
         card.style.transform = `translate(${x}px, ${y}px)`;
     });
 });
+
+// Auto-hide after 7 seconds
+setTimeout(() => {
+
+    const container =
+        document.getElementById(
+            "messages-container"
+        );
+
+    if(container){
+
+        container.style.transition =
+            "opacity .5s ease";
+
+        container.style.opacity = "0";
+
+        setTimeout(() => {
+            container.remove();
+        }, 500);
+    }
+
+}, 7000);
+
+
+// Close when X is clicked
+document.querySelectorAll(
+    ".close-message"
+).forEach(button => {
+
+    button.addEventListener(
+        "click",
+        function(){
+
+            const message =
+                this.closest(".message");
+
+            message.style.transition =
+                "opacity .3s ease";
+
+            message.style.opacity = "0";
+
+            setTimeout(() => {
+                message.remove();
+            }, 300);
+        }
+    );
+
+});
